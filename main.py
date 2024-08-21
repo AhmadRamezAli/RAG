e5faf39ad4ba9d2bdd4665bceabfa01209011c24
AhmadRamezAli <ahmad.70sahar@gmail.com>
2024-08-21 19:00:47 +0300

    some_edits

--- main.py
+++ main.py
@@ -1,5 +1,16 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from my_api.initialize_api import router
 app=FastAPI()
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Allow any origin
+    allow_credentials=True,
+    allow_methods=["*"],  # Allow all HTTP methods
+    allow_headers=["*"],  # Allow all headers
+)
+
 
 app.include_router(router)
--- my_api/initialize_api.py
+++ my_api/initialize_api.py
@@ -1,14 +1,51 @@
-from fastapi import FastAPI,Body
+from fastapi import FastAPI,Body, File, Form, UploadFile
 from fastapi import APIRouter
 from rag_core.get_answer_from_model import get_answer_from_model
-
+from pydantic import BaseModel
+from typing import Optional
 router = APIRouter(prefix="/api")
 
+
+class RequestModel(BaseModel):
+    chunks: int
+    numofresults: int
+    question: str
+    file:UploadFile
 @router.get("/")
 def root():
     return {"message": "Hello tt"}
 
+@router.post("/uploadfile/")
+async def postupload(
+    file: UploadFile
+):
+    print(f"Received file: {file.filename}, size: {file.size} bytes")
+    file_location = f"rag_core/src/{file.filename}"
+
+    try:
+        # Save the file
+        with open(file_location, "wb") as f:
+            f.write(await file.read())
+        
+        print(f"File saved to: {file_location}")
+        return  {"message": "file saved saccussefully"}
+    
+    except Exception as e:
+        print(f"Error occurred: {e}")
+        return {"message": "Error processing file", "error": str(e)}
+    
+
 
 @router.post("/query/")
-def postq(payload: dict = Body(...)):
-    return get_answer_from_model(r"rag_core/src/aram_mohammed.pdf",payload['chunks'],payload['numofresults'],payload['question'])
+async def postq(
+    chunks: int = Form(...),
+    numofresults: int = Form(...),
+    question: str = Form(...),
+    filepath: str = Form(...),
+):
+    file_locat= "rag_core/src/"+filepath
+    print(file_locat)
+    result = get_answer_from_model(file_locat, chunks, numofresults, question)
+        
+    return  result
+ 
